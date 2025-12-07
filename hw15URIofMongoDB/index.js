e66a7025e9f6c2afac9c7c326494cf822197432b
Kerlly Alexandra Chiriboga Cabrera <chiribogakerly@gmail.com>
2025-12-07 15:45:09 -0500

    env

--- hw15URIofMongoDB/index.js
+++ hw15URIofMongoDB/index.js
@@ -1,17 +1,30 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-app.use(express.json());
+const port = process.env.PORT || 3009;
 
 mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log("Connected to MongoDB"))
-    .catch(err => console.error("Mongo connection error:", err));
+    .then(() => console.log("Connected to Database"))
+    .catch(err => console.error(err));
 
+app.use(express.json());
 
-const recipeRoutes = require("./routes/recipeRoutes");
-app.use("/dishdash", recipeRoutes);
+const recipeRoutes = require('./routes/recipeRoutes');
+app.use('/dishdash', recipeRoutes);
 
-const port = process.env.PORT || 3009;
+app.get('/', (req, res) => {
+    res.json({
+        message: "DishDash Recipe API",
+        version: "1.0.0",
+        endpoints: {
+            "GET /dishdash/recipes": "Get all recipes",
+            "GET /dishdash/recipes/:id": "Get recipe by ID",
+            "POST /dishdash/recipe": "Create new recipe",
+            "PUT /dishdash/recipe/:id": "Update recipe",
+            "DELETE /dishdash/recipe/:id": "Soft delete recipe",
+        }
+    });
+});
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
